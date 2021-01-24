96659d70b0eaafa38b9b8139b1cbbfe63d80fd0b
tatsuaki <tatsuakikuroda@me.com>
2021-01-24 21:31:16 +0900

    wakuEye wakuFaceの設定

--- Fushiki/PlayViewController.swift
+++ Fushiki/PlayViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 import AVFoundation
 import Photos
+@available(iOS 11.0, *)
 class PlayViewController: UIViewController {
 
     var videoPlayer: AVPlayer!
@@ -19,23 +20,124 @@ class PlayViewController: UIViewController {
     var videoURL:URL?
     @IBOutlet weak var eyeWaku_image: UIImageView!
     @IBOutlet weak var faceWaku_image: UIImageView!
-    var wakuE = CGRect(x:300.0,y:100.0,width:5.0,height:5.0)
-    var wakuF = CGRect(x:300.0,y:200.0,width:5.0,height:5.0)
+    
+    
+    @IBOutlet weak var faceWakuL_image: UIImageView!
+    @IBOutlet weak var eyeWakuL_image: UIImageView!
+    
+    
+    var wakuEyeRect = CGRect(x:300.0,y:100.0,width:5.0,height:5.0)
+    var wakuFaceRect = CGRect(x:300.0,y:200.0,width:5.0,height:5.0)
+    func resizeR2(_ targetRect:CGRect, viewRect:CGRect, image:CIImage) -> CGRect {
+        //view.frameとtargetRectとimageをもらうことでその場で縦横の比率を計算してtargetRectのimage上の位置を返す関数
+        //view.frameとtargetRectは画面上の位置だが、返すのはimage上の位置なので、そこをうまく考慮する必要がある。
+        //getRealrectの代わり
+        
+        let vw = viewRect.width
+        let vh = viewRect.height
+        
+        let iw = CGFloat(image.extent.width)
+        let ih = CGFloat(image.extent.height)
+        
+        //　viewRect.originを引く事でtargetRectがview.bounds起点となる (xは0なのでやる必要はないが・・・）
+        let tx = CGFloat(targetRect.origin.x) - CGFloat(viewRect.origin.x)
+        let ty = CGFloat(targetRect.origin.y) - CGFloat(viewRect.origin.y)
+        
+        let tw = CGFloat(targetRect.width)
+        let th = CGFloat(targetRect.height)
+        
+        // ここで返されるCGRectはCIImage/CGImage上の座標なので全て整数である必要がある
+        // 端数があるまま渡すとmatchingが誤動作した
+        return CGRect(x: (tx * iw / vw).rounded(),
+                      y: ((vh - ty - th) * ih / vh).rounded(),
+                      width: (tw * iw / vw).rounded(),
+                      height: (th * ih / vh).rounded())
+    }
+    func showWakuImages(){//結果が表示されていない時、画面上部1/4をタップするとWaku表示
+        let options = [CIDetectorAccuracy: CIDetectorAccuracyHigh]
+        let avAsset = AVURLAsset(url: videoURL!, options: options)
+        var reader: AVAssetReader! = nil
+        do {
+            reader = try AVAssetReader(asset: avAsset)
+        } catch {
+            #if DEBUG
+            print("could not initialize reader.")
+            #endif
+            return
+        }
+        guard let videoTrack = avAsset.tracks(withMediaType: AVMediaType.video).last else {
+            #if DEBUG
+            print("could not retrieve the video track.")
+            #endif
+            return
+        }
+        
+        let readerOutputSettings: [String: Any] = [kCVPixelBufferPixelFormatTypeKey as String : Int(kCVPixelFormatType_420YpCbCr8BiPlanarFullRange)]
+        let readerOutput = AVAssetReaderTrackOutput(track: videoTrack, outputSettings: readerOutputSettings)
+        
+        reader.add(readerOutput)
+        let frameRate = videoTrack.nominalFrameRate
+        let startFrame:Int=0
+        //let startframe=startPoints[vhitVideocurrent]
+        let startTime = CMTime(value: CMTimeValue(startFrame), timescale: CMTimeScale(frameRate))
+        let timeRange = CMTimeRange(start: startTime, end:CMTime.positiveInfinity)
+        //print("time",timeRange)
+        reader.timeRange = timeRange //読み込む範囲を`timeRange`で指定
+        reader.startReading()
+        
+        let CGeye:CGImage!//eye
+        let UIeye:UIImage!
+        var CGface:CGImage!//face
+        var UIface:UIImage!
+        let context:CIContext = CIContext.init(options: nil)
+        let orientation = UIImage.Orientation.up//right
+        var sample:CMSampleBuffer!
+        sample = readerOutput.copyNextSampleBuffer()
+        let pixelBuffer: CVPixelBuffer = CMSampleBufferGetImageBuffer(sample!)!
+        let ciImage = CIImage(cvPixelBuffer: pixelBuffer).oriented(CGImagePropertyOrientation.right)
+        //起動時表示が一巡？するまでは　slowImage.frame はちょっと違う値を示す
+        let eyeRect = resizeR2(wakuEyeRect, viewRect:view.frame,image:ciImage)
+        CGeye = context.createCGImage(ciImage, from: eyeRect)!
+        UIeye = UIImage.init(cgImage: CGeye, scale:1.0, orientation:orientation)
+        eyeWakuL_image.frame=CGRect(x:5,y:5,width: eyeRect.size.width*5,height: eyeRect.size.height*5)
+        eyeWakuL_image.layer.borderColor = UIColor.black.cgColor
+        eyeWakuL_image.layer.borderWidth = 1.0
+        eyeWakuL_image.backgroundColor = UIColor.clear
+        eyeWakuL_image.layer.cornerRadius = 3
+        eyeWakuL_image.image=UIeye
+        view.bringSubviewToFront(eyeWakuL_image)
+
+        let faceRect = resizeR2(wakuFaceRect, viewRect:view.frame, image: ciImage)
+        CGface = context.createCGImage(ciImage, from: faceRect)!
+        UIface = UIImage.init(cgImage: CGface, scale:1.0, orientation:orientation)
+        faceWakuL_image.frame=CGRect(x:view.bounds.width - faceRect.size.width*5 - 5,y:5,width: faceRect.size.width*5,height: faceRect.size.height*5)
+        faceWakuL_image.layer.borderColor = UIColor.black.cgColor
+        faceWakuL_image.layer.borderWidth = 1.0
+        faceWakuL_image.backgroundColor = UIColor.clear
+        faceWakuL_image.layer.cornerRadius = 3
+        faceWakuL_image.image=UIface
+        view.bringSubviewToFront(faceWakuL_image)
+    }
+
     func dispWakus(){
         let nullRect:CGRect = CGRect(x:0,y:0,width:0,height:0)
         //        printR(str:"wakuE:",rct: wakuE)
-        eyeWaku_image.frame=CGRect(x:(wakuE.origin.x)-15,y:wakuE.origin.y-15,width:(wakuE.size.width)+30,height: wakuE.size.height+30)
-        faceWaku_image.frame=CGRect(x:(wakuF.origin.x)-15,y:wakuF.origin.y-15,width:wakuF.size.width+30,height: wakuF.size.height+30)
+        eyeWaku_image.frame=CGRect(x:(wakuEyeRect.origin.x)-15,y:wakuEyeRect.origin.y-15,width:(wakuEyeRect.size.width)+30,height: wakuEyeRect.size.height+30)
+        faceWaku_image.frame=CGRect(x:(wakuFaceRect.origin.x)-15,y:wakuFaceRect.origin.y-15,width:wakuFaceRect.size.width+30,height: wakuFaceRect.size.height+30)
         
         eyeWaku_image.layer.borderColor = UIColor.green.cgColor
         eyeWaku_image.backgroundColor = UIColor.clear
-        eyeWaku_image.layer.borderWidth = 1.0
-        eyeWaku_image.layer.cornerRadius = 3
-        
+        eyeWaku_image.layer.cornerRadius = 4
         faceWaku_image.layer.borderColor = UIColor.green.cgColor
         faceWaku_image.backgroundColor = UIColor.clear
-        faceWaku_image.layer.borderWidth = 1.0
-        faceWaku_image.layer.cornerRadius = 3
+        faceWaku_image.layer.cornerRadius = 4
+        if wakuType==0{
+            eyeWaku_image.layer.borderWidth = 2
+            faceWaku_image.layer.borderWidth = 1
+        }else{
+            eyeWaku_image.layer.borderWidth = 1
+            faceWaku_image.layer.borderWidth = 2
+        }
         view.bringSubviewToFront(faceWaku_image)
         view.bringSubviewToFront(eyeWaku_image)
     }
@@ -61,52 +163,44 @@ class PlayViewController: UIViewController {
         }
         return r
     }
-    var leftrightFlag:Bool = false
-    var rectType:Int = 0//0:eye 1:face 2:outer -1:何も選択されていない
-    var stPo:CGPoint = CGPoint(x:0,y:0)//stRect.origin tapした位置
-    var stRect:CGRect = CGRect(x:0,y:0,width:0,height:0)//tapしたrectのtapした時のrect
-    var changePo:CGPoint = CGPoint(x:0,y:0)
-    var endPo:CGPoint = CGPoint(x:0,y:0)
-    var lastslowVideo:Int = -2
-    var lastVogpoint:Int = -2
-    var lastVhitpoint:Int = -2
-    var lastmoveX:Int = -2
-    var lastmoveXgyro:Int = -2//vHIT用
-    @IBAction func panGesture(_ sender: UIPanGestureRecognizer) {
 
+    var wakuType:Int = 0//0:eye 1:face 2:outer -1:何も選択されていない
+    var startPoint:CGPoint = CGPoint(x:0,y:0)//stRect.origin tapした位置
+    var startEyeRect:CGRect = CGRect(x:0,y:0,width:0,height:0)//tapしたrectのtapした時のrect
+    var startFaceRect:CGRect = CGRect(x:0,y:0,width:0,height:0)
+    @IBAction func panGesture(_ sender: UIPanGestureRecognizer) {
         let move:CGPoint = sender.translation(in: self.view)
-        let pos = sender.location(in: self.view)
         if sender.state == .began {
-            stPo = sender.location(in: self.view)
+            startPoint = sender.location(in: self.view)
+            startEyeRect=wakuEyeRect
+            startFaceRect=wakuFaceRect
         } else if sender.state == .changed {
             
             let ww=view.bounds.width
             let wh=view.bounds.height
-            if rectType == 0 {
-                if true {//EyeRect
-                    let et=CGRect(x:ww/10,y:wh/20,width: ww*4/5,height:wh*3/4)
-                    wakuE = moveWakus(rect:wakuE,stRect: stRect,stPo: stPo,movePo: move,hani: et)
-                }else{//vHIT && faceF==true FaceRect
-                    let et=CGRect(x:ww/10,y:wh/20,width: ww*4/5,height:wh*3/4)
-                    wakuE = moveWakus(rect:wakuE,stRect: stRect,stPo: stPo,movePo: move,hani:et)
-                }
+            
+            let et=CGRect(x:ww/10,y:wh/20,width: ww*4/5,height:wh*3/4)
+            if wakuType==0{
+                wakuEyeRect = moveWakus(rect:wakuFaceRect,stRect:startEyeRect, stPo: startPoint,movePo: move,hani:et)
             }else{
-                //let xt=wakuE.origin.x
-                //let w12=view.bounds.width/12
-                let et=CGRect(x:ww/10,y:wh/20,width: ww*4/5,height:wh*3/4)
-                wakuF = moveWakus(rect:wakuF,stRect:stRect, stPo: stPo,movePo: move,hani:et)
+                wakuFaceRect = moveWakus(rect:wakuFaceRect,stRect:startFaceRect, stPo: startPoint,movePo: move,hani:et)
             }
             dispWakus()
-//            showWakuImages()
-   //         setUserDefaults()
-            
+            showWakuImages()
         }else if sender.state == .ended{
-            
-   //         setUserDefaults()
-            
         }
     }
     
+    @IBAction func tapGesture(_ sender: UITapGestureRecognizer) {
+        print("tap")
+        if wakuType==0{//eye
+            wakuType=1
+        }else{
+            wakuType=0
+        }
+        dispWakus()
+    }
+
     
     @objc func update(tm: Timer) {
         currTime?.text=String(format:"%.1f/%.1f",seekBar.value,duration)
