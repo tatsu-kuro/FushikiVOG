4b9313eab03008aa06b94f1178ceea3a953de653
tatsuaki <tatsuakikuroda@me.com>
2021-02-25 08:02:43 +0900

    iroiro

--- Fushiki/CameraViewController.swift
+++ Fushiki/CameraViewController.swift
@@ -54,11 +54,7 @@ class CameraViewController: UIViewController {
         focusBar.addTarget(self, action: #selector(onFocusValueChange), for: UIControl.Event.valueChanged)
         focusBar.value=camera.getUserDefaultFloat(str: "focusValue", ret: 0)
         camera.setFocus(focus: focusBar.value)
-        if cameraMode==2{
-            cameraView.alpha=0.1
-        }else{
-            cameraView.alpha=1
-        }
+      
     }
     
     @IBAction func onCameraChan(_ sender: UISegmentedControl) {
@@ -74,6 +70,7 @@ class CameraViewController: UIViewController {
         print("cameraMode:",cameraMode)
         fpsLabel.text = String(format:"fps:%d %dx%d" ,camera.fpsCurrent,camera.widthCurrent,camera.heightCurrent)
         camera.setLedLevel(level:camera.getUserDefaultFloat(str: "ledValue", ret:0))
+        setButtons()
     }
     @objc func onLedValueChange(){
         camera.setLedLevel(level:ledBar.value)
@@ -101,7 +98,8 @@ class CameraViewController: UIViewController {
         let bw=(ww-sp*10)/7//ボタン幅
         let bh=bw*170/440
         let by=wh-bh-sp
-    
+        let cameraMode = Int(camera.getUserDefaultInt(str: "cameraMode", ret: 0))
+
         camera.setButtonProperty(exitButton,x:bw*6+sp*8,y:by,w:bw,h:bh,UIColor.darkGray)
         fpsButton.isHidden=true
         camera.setLabelProperty( fpsLabel,x:bw*1+sp*4,y:by,w:bw*2,h:bh,UIColor.white)
@@ -112,6 +110,30 @@ class CameraViewController: UIViewController {
         focusBar.frame=CGRect(x:5*sp,y:by-sp*2/3-2*bh,width:ww-7*sp-bw,height:bh)
         zoomBar.frame=CGRect(x:5*sp,y:by-sp/3-bh,width:ww-7*sp-bw,height:bh)
         cameraChan.frame=CGRect(x:bw*3+sp*5,y:by,width:bw*3+sp*2,height:bh)
+        if cameraMode==2{
+            cameraView.alpha=0.1
+            focusBar.isHidden=true
+            focusLabel.isHidden=true
+            ledBar.isHidden=true
+            ledLabel.isHidden=true
+            zoomBar.isHidden=true
+            zoomLabel.isHidden=true
+        }else if cameraMode==1{
+            cameraView.alpha=1
+            focusBar.isHidden=false
+            focusLabel.isHidden=false
+            ledBar.isHidden=false
+            ledLabel.isHidden=false
+            zoomBar.isHidden=false
+            zoomLabel.isHidden=false
+        }else if cameraMode==0{
+            cameraView.alpha=1
+            focusBar.isHidden=true
+            focusLabel.isHidden=true
+            ledBar.isHidden=true
+            ledLabel.isHidden=true
+            zoomBar.isHidden=false
+            zoomLabel.isHidden=false        }
      }
 }
 
