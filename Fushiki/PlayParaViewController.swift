53b5b782247d28f3754d56d76010a15a7e193d89
tatsuaki <tatsuakikuroda@me.com>
2022-06-02 08:06:12 +0900

    button体裁修正

--- Fushiki/PlayParaViewController.swift
+++ Fushiki/PlayParaViewController.swift
@@ -229,17 +229,17 @@ class PlayParaViewController: UIViewController {
 
         let sp=ww/120//間隙
         let bw=(ww-sp*10)/7//ボタン幅
-        let bw_wide=(ww-bw-8*sp)/4
+//        let bw_wide=(ww-bw-8*sp)/4
         let bh=bw*170/440
         let by=wh-bh-sp
         let lw=ww-bw*2
         let head=sp*2+left
         camera.setButtonProperty(keyPadDownButton, x: head+bw*6+sp*6, y: sp, w: bw, h: bh, UIColor.darkGray)
         camera.setButtonProperty(exitButton,x:head+sp*6+bw*6,y:by,w:bw,h:bh,UIColor.darkGray)
-        camera.setButtonProperty(default1Button, x:head, y: by, w: bw_wide, h: bh, UIColor.darkGray)
-        camera.setButtonProperty(default2Button, x:head+sp+bw_wide, y: by, w: bw_wide, h: bh, UIColor.darkGray)
-        camera.setButtonProperty(default3Button, x:head+sp*2+bw_wide*2, y: by, w: bw_wide, h: bh, UIColor.darkGray)
-        camera.setButtonProperty(defaultButton, x:head+sp*3+bw_wide*3, y: by, w: bw_wide, h: bh, UIColor.darkGray)
+//        camera.setButtonProperty(default1Button, x:head, y: by, w: bw_wide, h: bh, UIColor.darkGray)
+//        camera.setButtonProperty(default2Button, x:head+sp+bw_wide, y: by, w: bw_wide, h: bh, UIColor.darkGray)
+//        camera.setButtonProperty(default3Button, x:head+sp*2+bw_wide*2, y: by, w: bw_wide, h: bh, UIColor.darkGray)
+        camera.setButtonProperty(defaultButton, x:head+sp*5+bw*5, y: by, w: bw, h: bh, UIColor.darkGray)
         para1.frame=CGRect(x:head,y:sp,width:bw,height: bh)
         para2.frame=CGRect(x:head,y:sp*2+bh,width:bw,height: bh)
         para3.frame=CGRect(x:head,y:sp*3+bh*2,width:bw,height: bh)
